5a308a6586eae9e539d38b79a66b2a7726c9f695
realstoman <realstoman@gmail.com>
2026-01-03 17:01:42 +0430

    Update dashboard logo and links

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,19 @@
-import AppLogoIcon from './app-logo-icon';
+import { brand } from '@/config/brand';
 
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground">
+                <img
+                    src={`${brand.logo}`}
+                    width="120"
+                    height="120"
+                    alt="Logo"
+                />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                <span className="truncate leading-none font-bold tracking-wide">
+                    Baba Restaurant
                 </span>
             </div>
         </>
