4d1f8b33d15920fcfbf9dfc0888422507f092e70
realstoman <realstoman@gmail.com>
2026-03-05 23:11:57 +0430

    Add types and update the table

---
+++ resources/js/pages/employees/index.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { EmployeeClient } from '@/components/tables/employees/client';
+import AppLayout from '@/layouts/app-layout';
+import { dashboard } from '@/routes';
+import {
+    Branch,
+    BreadcrumbItem,
+    Employee,
+    EmployeePosition,
+    EmploymentType,
+} from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard().url,
+    },
+    {
+        title: 'Employees',
+        href: '/employees',
+    },
+];
+
+interface EmployeesPageProps {
+    employees: Employee[];
+    branches: Branch[];
+    employmentTypes: EmploymentType[];
+    employeePositions: EmployeePosition[];
+}
+
+export default function EmployeesPage({
+    employees,
+    branches,
+    employmentTypes,
+    employeePositions,
+}: EmployeesPageProps) {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Employees" />
+            <div className="space-y-4 rounded-lg bg-white p-8 dark:bg-brand-bg-dark">
+                <div className="p-6 text-gray-900">
+                    <EmployeeClient
+                        data={employees}
+                        branches={branches}
+                        employmentTypes={employmentTypes}
+                        employeePositions={employeePositions}
+                    />
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
