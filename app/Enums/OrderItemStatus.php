2a81aae71a2a3abbd9658ce929690a317a8781e3
realstoman <realstoman@gmail.com>
2026-03-07 22:28:57 +0430

    Update models

--- app/Enums/OrderItemStatus.php
+++ app/Enums/OrderItemStatus.php
@@ -9,4 +9,9 @@ enum OrderItemStatus: string
     case READY = 'ready';
     case SERVED = 'served';
     case CANCELLED = 'cancelled';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
--- app/Enums/OrderType.php
+++ app/Enums/OrderType.php
@@ -7,4 +7,9 @@ enum OrderType: string
     case DINE_IN = 'dine_in';
     case TAKEAWAY = 'takeaway';
     case DELIVERY = 'delivery';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
