837cd6c8cfed41a754ba6e8ab7ae652909627c2a
realstoman <realstoman@gmail.com>
2026-03-14 23:54:22 +0430

    Add phase 1 models

---
+++ app/Models/Client.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    protected $fillable = [
+        'firebase_uid',
+        'email',
+        'phone',
+        'name',
+        'avatar_url',
+        'provider',
+        'email_verified_at',
+        'last_login_at',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'last_login_at' => 'datetime',
+        'is_active' => 'boolean',
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
