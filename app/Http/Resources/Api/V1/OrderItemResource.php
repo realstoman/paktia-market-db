446b047cd97ba6abdf95dcfe0337bbd85749ee85
realstoman <realstoman@gmail.com>
2026-03-15 00:53:08 +0430

    Update resources and orders

--- app/Http/Resources/Api/V1/OrderItemResource.php
+++ app/Http/Resources/Api/V1/OrderItemResource.php
@@ -12,16 +12,17 @@ class OrderItemResource extends JsonResource
         return [
             'id' => $this->id,
             'product_id' => $this->product_id,
-            'product_name' => $this->product?->name,
+            'product_name' => $this->product_name_snapshot ?? $this->product?->name,
             'product_category_id' => $this->product?->product_category_id,
             'product_category_name' => $this->product?->category?->name,
             'product_size_id' => $this->product_size_id,
-            'product_size_name' => $this->productSize?->name,
+            'product_size_name' => $this->product_size_name_snapshot ?? $this->productSize?->name,
             'kitchen_id' => $this->kitchen_id,
             'kitchen_name' => $this->kitchen?->name,
             'quantity' => (int) $this->quantity,
             'price' => (float) $this->price,
-            'line_total' => (float) ($this->price * $this->quantity),
+            'line_total' => (float) ($this->line_total ?? ($this->price * $this->quantity)),
+            'note' => $this->note,
         ];
     }
 }
