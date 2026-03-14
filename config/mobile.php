a4844200454d8885f64b083d580521f024ca8edf
realstoman <realstoman@gmail.com>
2026-03-14 23:54:04 +0430

    Add mobile route layer

---
+++ config/mobile.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'apps' => [
+        [
+            'key' => env('MOBILE_APP_KEY_IOS'),
+            'platform' => 'ios',
+            'active' => true,
+        ],
+        [
+            'key' => env('MOBILE_APP_KEY_ANDROID'),
+            'platform' => 'android',
+            'active' => true,
+        ],
+    ],
+
+    'guest' => [
+        'expires_in_days' => (int) env('MOBILE_GUEST_EXPIRES_IN_DAYS', 30),
+    ],
+
+    'firebase' => [
+        'stub_mode' => (bool) env('MOBILE_FIREBASE_STUB_MODE', app()->environment(['local', 'testing'])),
+    ],
+];
