6a40f6adab66d01963c32dc8d1e2c736d7fedee4
realstoman <realstoman@gmail.com>
2026-05-07 23:08:43 +0430

    Make digital menu api endpoints to be public

--- app/Http/Controllers/Api/V1/DigitalTabletMenuController.php
+++ app/Http/Controllers/Api/V1/DigitalTabletMenuController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\DigitalTabletMenuProductResource;
+use App\Http\Resources\Api\V1\ProductCategoryResource;
+use App\Http\Resources\Api\V1\ProductTypeResource;
+use App\Models\ProductCategory;
+use App\Models\ProductType;
 use App\Services\Product\DigitalTabletMenuProductService;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -13,5 +17,35 @@ class DigitalTabletMenuController extends Controller
         DigitalTabletMenuProductService $service,
     ): AnonymousResourceCollection {
         return DigitalTabletMenuProductResource::collection($service->all());
+    }
+
+    public function categories(
+        DigitalTabletMenuProductService $service,
+    ): AnonymousResourceCollection {
+        return ProductCategoryResource::collection($service->categories());
+    }
+
+    public function productsByCategory(
+        ProductCategory $category,
+        DigitalTabletMenuProductService $service,
+    ): AnonymousResourceCollection {
+        return DigitalTabletMenuProductResource::collection(
+            $service->productsByCategory($category)
+        );
+    }
+
+    public function types(
+        DigitalTabletMenuProductService $service,
+    ): AnonymousResourceCollection {
+        return ProductTypeResource::collection($service->types());
+    }
+
+    public function productsByType(
+        ProductType $type,
+        DigitalTabletMenuProductService $service,
+    ): AnonymousResourceCollection {
+        return DigitalTabletMenuProductResource::collection(
+            $service->productsByType($type)
+        );
     }
 }
