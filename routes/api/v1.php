1a9dea95977a1a29af89fcbd2d21fb38df5430de
realstoman <realstoman@gmail.com>
2026-05-07 23:06:30 +0430

    Make the banner and digital menu public

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -13,14 +13,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('orders', [OrderController::class, 'index'])->name('orders.index');
 Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
+Route::get('banners', [BannerController::class, 'index'])->name('banners.index');
+Route::get('banners/{banner}', [BannerController::class, 'show'])->name('banners.show');
+
+Route::get('digital-tablet-menu/products', [DigitalTabletMenuController::class, 'products'])
+    ->name('digital-tablet-menu.products.index');
 
 Route::middleware('app.auth')->group(function (): void {
-    Route::get('banners', [BannerController::class, 'index'])->name('banners.index');
-    Route::get('banners/{banner}', [BannerController::class, 'show'])->name('banners.show');
-
-    Route::get('digital-tablet-menu/products', [DigitalTabletMenuController::class, 'products'])
-        ->name('digital-tablet-menu.products.index');
-
     Route::get('products/categories', [ProductController::class, 'categories'])->name('products.categories.index');
     Route::get('products/categories/{category}', [ProductController::class, 'showCategory'])->name('products.categories.show');
     Route::get('products/categories/{category}/products', [ProductController::class, 'productsByCategory'])->name('products.categories.products');
