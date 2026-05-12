0247bb27ca9b981cda72644f616e91ff0f8df7dd
realstoman <realstoman@gmail.com>
2026-05-12 10:42:38 +0430

    add client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Client extends Model
+class Client extends Authenticatable
 {
     protected $fillable = [
         'firebase_uid',
