d6ead609b85f5529f9f72893afd0ab2674cbbd35
realstoman <realstoman@gmail.com>
2026-06-23 15:12:05 +0430

    Design changes in the show page

--- resources/js/pages/shareholders/show.tsx
+++ resources/js/pages/shareholders/show.tsx
@@ -18,7 +18,6 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { Textarea } from '@/components/ui/textarea';
 import AppLayout from '@/layouts/app-layout';
 import { useLocalization } from '@/lib/localization';
 import {
